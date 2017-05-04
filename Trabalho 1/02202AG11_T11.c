5280877fd006113476eb54941aec1b5834643d09
gabrielgmartins <gabrielsteammer@hotmail.com>
2017-05-03 21:27:26 -0300

    Update 02202AG11_T11.c

--- Trabalho 1/02202AG11_T11.c
+++ Trabalho 1/02202AG11_T11.c
@@ -1,17 +1,9 @@
-/* 	Gabriel Germino Martins de Jesus	16200529
-	Anderson Alves Marzani				16200518
-	Turma: 	2202A
-	Grupo:	11
-	Tarefa01_2017_1
-	Exercício 1 */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
 main() {
 	int n, b, c;
-	n = 0; // valor é declarado para que seja possivel entrar no loop
+	n = 0; // valor Ã© declarado para que seja possivel entrar no loop
 	printf ("\t\t%c Tri%cngulo do n%cmero m%cximo %c\n\n", 254, 134, 163, 160, 254);
 	while (n%2 != 1) { 
 		printf ("Entre com o n%cmero m%cximo, que deve ser %cmpar: ", 163, 160, 161);
